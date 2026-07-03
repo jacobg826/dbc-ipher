cec4741b4551c0b8ed0132fe38898d6bec2c8da5
jacob gervais <gervais.jacob@gmail.com>
2026-07-03 08:40:34 -0700

    feat: add signal names to detail view

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use crossterm::event::{self, KeyCode};
-use ratatui::layout::{Constraint, Direction, Layout, Rect};
+use ratatui::layout::{Constraint, Direction, Layout, Rect, Spacing};
 use ratatui::style::{Color, Modifier, Style, Stylize};
+use ratatui::symbols::merge::MergeStrategy;
 use ratatui::text::{Line, Span};
 use ratatui::widgets::{Block, Borders, Paragraph};
 use ratatui::widgets::{List, ListDirection, ListState};
@@ -90,7 +91,17 @@ fn render_list<T: Listable>(
 
 fn render_detail(frame: &mut Frame, area: Rect, message: Option<&dbc_rs::Message>) {
     if let Some(msg) = message {
-        render_message_header(frame, area, msg);
+        let outer_block = Block::default().borders(Borders::ALL);
+        let inner_area = outer_block.inner(area);
+
+        let [msg_area, sig_area] = Layout::vertical([
+            Constraint::Length(8), // fixed height for header
+            Constraint::Fill(1),   // signals get the rest
+        ])
+        .areas(inner_area);
+
+        render_message_header(frame, msg_area, msg);
+        render_signal_list(frame, sig_area, msg);
     }
 }
 
@@ -101,7 +112,7 @@ fn render_message_header(frame: &mut Frame, area: Rect, msg: &dbc_rs::Message) {
     let da = (id >> 8) & 0xFF;
     let priority = (id >> 26) & 0x7;
 
-    let label_width = 10; // tune once, applies everywhere
+    let label_width = 10;
     let col_width = 16;
 
     let lines = vec![
@@ -154,6 +165,21 @@ fn render_message_header(frame: &mut Frame, area: Rect, msg: &dbc_rs::Message) {
 
     frame.render_widget(
         Paragraph::new(lines).block(Block::default().borders(Borders::ALL).title("Message")),
+        area,
+    );
+}
+
+fn render_signal_list(frame: &mut Frame, area: Rect, msg: &dbc_rs::Message) {
+    let label_width = 10;
+    let sig_names: Vec<Line> = msg
+        .signals()
+        .iter()
+        .map(|sig| {
+            return Line::from(vec![Span::raw(sig.name())]);
+        })
+        .collect();
+    frame.render_widget(
+        Paragraph::new(sig_names).block(Block::default().borders(Borders::ALL).title("Signals")),
         area,
     );
 }
